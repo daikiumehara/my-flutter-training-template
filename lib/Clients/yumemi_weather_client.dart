46bc5655f7e48db6d2e21b4a3c4e641fca3093cb
daikiumehara <daikiumehara3@gmail.com>
2026-03-05 17:27:46 +0900

    Clientの追加

---
+++ lib/Clients/yumemi_weather_client.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+import '../Datas/weather_data.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+
+interface class YumemiWeatherClient {
+  Future<WeatherData> fetchWeatherData() async {
+    return .new(.sunny);  
+  }
+}
+
+class YumemiWeatherClientImpl implements YumemiWeatherClient {
+  @override
+  Future<WeatherData> fetchWeatherData() async {
+    final yumemiWeather = YumemiWeather();
+    final weatherCondition = yumemiWeather.fetchSimpleWeather();
+    final weatherType = WeatherType.fromString(weatherCondition);
+    if(weatherType == null) {
+      return .new(.sunny);
+    }
+    return .new(weatherType);
+  }
+}
