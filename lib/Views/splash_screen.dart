788645ebba83c9901fda6cebe0a1af87b6d41eb1
daikiumehara <daikiumehara3@gmail.com>
2026-03-11 14:06:14 +0900

    SplashStateのmixin作成

--- lib/Views/splash_screen.dart
+++ lib/Views/splash_screen.dart
@@ -3,29 +3,36 @@ import 'package:flutter_training/Clients/yumemi_weather_client.dart';
 import 'package:flutter_training/Views/weather_detail_screen.dart';
 
 class SplashScreen extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return SplashState();
   }
 }
 
-class SplashState extends State<SplashScreen> with RouteAware {
+mixin SplashStateInitStateMixin on State<SplashScreen> {
   @override
   void initState() {
     super.initState();
 
     Future(() async {
       await WidgetsBinding.instance.endOfFrame;
-      _navigateToWeatherDetail();
+      await navigateToMain();
     });
   }
 
-  void _navigateToWeatherDetail() async {
-    await Future.delayed(Duration(milliseconds: 500));
+  Future<void> navigateToMain();
+}
+
+class SplashState extends State<SplashScreen> with RouteAware, SplashStateInitStateMixin {
+  @override
+  Future<void> navigateToMain() async {
+    await Future<void>.delayed(const Duration(milliseconds: 500));
+
+    if (!mounted) return;
+
     await Navigator.push(
       context,
-      MaterialPageRoute(
+      MaterialPageRoute<void>(
         builder: (context) {
           return WeatherdetailScreen(
             weatherClient: YumemiWeatherClientImpl(),
@@ -34,7 +41,7 @@ class SplashState extends State<SplashScreen> with RouteAware {
       ),
     );
 
-    _navigateToWeatherDetail();
+    await navigateToMain();
   }
 
   @override
