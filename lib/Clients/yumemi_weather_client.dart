90f89ddd4ebacb4ea0790714bcece879bcc69d20
daikiumehara <daikiumehara3@gmail.com>
2026-03-12 17:55:25 +0900

    新しいAPIを使用するように修正

--- lib/Clients/yumemi_weather_client.dart
+++ lib/Clients/yumemi_weather_client.dart
@@ -2,22 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 import '../Datas/weather_data.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
+import 'dart:convert' as convert;
 
-interface class YumemiWeatherClient {
-  Future<WeatherData> fetchWeatherData() async {
-    return .new(.sunny);  
-  }
+mixin YumemiWeatherClient {
+  Future<WeatherData> fetchWeatherData();
 }
 
-class YumemiWeatherClientImpl implements YumemiWeatherClient {
+class YumemiWeatherClientImpl with YumemiWeatherClient {
   @override
   Future<WeatherData> fetchWeatherData() async {
     final yumemiWeather = YumemiWeather();
-    final weatherCondition = yumemiWeather.fetchThrowsWeather('tokyo');
-    final weatherType = WeatherType.fromString(weatherCondition);
-    if(weatherType == null) {
-      return .new(.sunny);
-    }
-    return .new(weatherType);
+    final requestData = WeatherRequestData('tokyo', .now());
+    final params = convert.json.encode(requestData.toJson());
+    final json = yumemiWeather.fetchWeather(params);
+    final map = convert.json.decode(json) as Map<String, dynamic>;
+    final weatherData = new WeatherData.fromJson(map);
+    return weatherData;
   }
 }
