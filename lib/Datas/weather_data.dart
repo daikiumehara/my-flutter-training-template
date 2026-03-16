044c535acaf3a51fae7c6a3437d38a7aa311e4dc
daikiumehara <daikiumehara3@gmail.com>
2026-03-16 14:10:08 +0900

    json_serializableを使用する形に修正

--- lib/Datas/weather_data.dart
+++ lib/Datas/weather_data.dart
@@ -1,45 +1,38 @@
 import 'package:yumemi_weather/yumemi_weather.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'weather_data.g.dart';
+
+@JsonSerializable(createJsonSchema: true)
 class WeatherRequestData {
+  WeatherRequestData(this.area, this.date);
+
   String area;
   DateTime date;
 
-  WeatherRequestData(this.area, this.date);
-
-  Map<String, dynamic> toJson() {
-    return {
-      'area': area,
-      'date': date.toIso8601String(),
-    };
-  }
+  Map<String, dynamic> toJson() => _$WeatherRequestDataToJson(this);
 }
 
+@JsonSerializable(createJsonSchema: true)
 class WeatherData {
-  WeatherType weatherType;
-  int maxTemp;
-  int minTemp;
-  String date;
-
   WeatherData(this.weatherType, this.maxTemp, this.minTemp, this.date);
 
-  WeatherData.fromJson(Map<String, dynamic> json)
-    : weatherType = WeatherType.fromString(json['weather_condition'] as String) ?? .sunny, // MEMO：一旦sunnyを返す
-      maxTemp = json['max_temperature'] as int,
-      minTemp = json['min_temperature'] as int,
-      date = json['date'] as String;
+  factory WeatherData.fromJson(Map<String, dynamic> json) => _$WeatherDataFromJson(json);
+
+  @JsonKey(name: 'weather_condition')
+  WeatherType weatherType;
+  @JsonKey(name: 'max_temperature')
+  int maxTemp;
+  @JsonKey(name: 'min_temperature')
+  int minTemp;
+  String date;
 }
 
 enum WeatherType { 
+  @JsonValue('sunny')
   sunny, 
+  @JsonValue('cloudy')
   cloudy,
+  @JsonValue('rainy')
   rainy;
-
-  static WeatherType? fromString(String value) {
-    return switch(value) {
-      'sunny' => .sunny,
-      'cloudy' => .cloudy,
-      'rainy' => .rainy,
-      _ => null
-    };
-  }
 }
